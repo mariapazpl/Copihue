b45e7c6d99f746a1e8033e421a9be557a93496a8
mariapazpl <maria@paredes@torontomu.ca>
2024-08-29 19:28:58 -0400

    debugging booking form mobile

--- src/pages/api/classes.ts
+++ src/pages/api/classes.ts
@@ -6,6 +6,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       const { username } = req.query;
 
+      console.log('Requesting:', `/api/classes?username=${username}`);
+
       if (!username || typeof username !== 'string') {
         console.log('Invalid or missing username in query:', username);
         return res.status(400).json({ message: 'Username is required' });
